98db3f8472a8200494beaf2d695747b4c6edc045
Tolnai Zoltán <tolnaiz@gmail.com>
2015-05-09 11:28:46 +0200

    some extra tests for the given examples

--- ruby-numerals/test/controllers/numerals_controller_test.rb
+++ ruby-numerals/test/controllers/numerals_controller_test.rb
@@ -13,11 +13,29 @@ class NumeralsControllerTest < ActionController::TestCase
     end
 
     test "post index with parameter" do
-      post :index, numeral: 5
+      post :index, numeral: 7
       assert_response :success
       assert_not_nil assigns(:humanized)
       assert_not_nil assigns(:numeral)
-      assert_equal "5", assigns(:numeral)
-      assert_equal "five", assigns(:humanized)
+      assert_equal "7", assigns(:numeral)
+      assert_equal "seven", assigns(:humanized)
+    end
+
+    # given test numbers
+    test "post index with 42" do
+      post :index, numeral: 42
+      assert_equal "forty-two", assigns(:humanized)
+    end
+
+    test "post index with 2001" do
+      post :index, numeral: 2001
+      assert_equal "two thousand and one", assigns(:humanized)
+    end
+
+    # the description says "nineteen hundred and ninety-nine", but one thousand is also a 
+    # valid pronunciation
+    test "post index with 1999" do
+      post :index, numeral: 1999
+      assert_equal "one thousand, nine hundred and ninety-nine", assigns(:humanized)
     end
 end
