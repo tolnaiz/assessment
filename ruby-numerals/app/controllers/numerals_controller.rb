551ef61d408d43970cde82b39ec8fff05fc8f170
Tolnai Zoltán <tolnaiz@gmail.com>
2015-05-09 11:07:18 +0200

    showing the english phrase of the number with humanize

--- ruby-numerals/app/controllers/numerals_controller.rb
+++ ruby-numerals/app/controllers/numerals_controller.rb
@@ -1,5 +1,10 @@
+require 'humanize'
+
 class NumeralsController < ApplicationController
 
 	def index
+		@numeral = params[:numeral]
+		@humanized_show = ! @numeral.nil?
+		@humanized = @numeral.to_i.humanize
 	end
 end
--- ruby-numerals/test/controllers/numerals_controller_test.rb
+++ ruby-numerals/test/controllers/numerals_controller_test.rb
@@ -6,8 +6,18 @@ class NumeralsControllerTest < ActionController::TestCase
       assert_response :success
     end
 
-    test "post index" do
+    test "post index with empty value" do
       post :index
       assert_response :success
+      assert_not_nil assigns(:humanized)
+    end
+
+    test "post index with parameter" do
+      post :index, numeral: 5
+      assert_response :success
+      assert_not_nil assigns(:humanized)
+      assert_not_nil assigns(:numeral)
+      assert_equal "5", assigns(:numeral)
+      assert_equal "five", assigns(:humanized)
     end
 end
